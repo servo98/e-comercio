f32773b494d9c639e52dd9cba6f027b486679654
servo pc <fernandonando98@live.com.mx>
2025-04-30 20:46:05 -0600

    feat: Express added to backend

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import http from "http";
+import api from "./api.js";
 
-const server = http.createServer();
+const server = http.createServer(api);
 
 server.on("listening", () => {
   console.info("Server running on port 8080");
