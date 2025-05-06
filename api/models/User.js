ddf6df5c90f72587fef7679d9118439055ad145b
servo pc <fernandonando98@live.com.mx>
2025-05-05 19:24:58 -0600

    feat: User model

---
+++ api/models/User.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model("User", userSchema);
